cd5d66895c4a90fa16dee7a8be5bb54ff30d2229
Raymond Ng <rayng86@gmail.com>
2018-02-24 18:06:43 -0500

    add pagination

--- src/main/Main.js
+++ src/main/Main.js
@@ -7,6 +7,10 @@ import Movies from "./movies/Movies";
 
 class Main extends React.Component {
  state = {
+   movies: [],
+   total_pages: 1,
+   page: 1,
+   url: `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`,
    moviesUrl: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`,
    genre: "Comedy",
    genres: [],
@@ -33,6 +37,19 @@ class Main extends React.Component {
    }
  }
 
+  componentDidMount() {
+   this.fetchMovies(this.state.moviesUrl);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (this.state.moviesUrl !== nextState.moviesUrl) {
+      this.fetchMovies(nextState.moviesUrl);
+    }
+    if (this.state.page !== nextState.page) {
+      this.generateUrl();
+    }
+  }
+
   setGenres = genres => {
     this.setState({genres});
   }
@@ -51,13 +68,12 @@ class Main extends React.Component {
   };
 
   generateUrl = () => {
-    const {genres, year, rating, runtime } = this.state;
+    const {genres, year, rating, runtime, page } = this.state;
     const selectedGenre = genres.find( genre => genre.name === this.state.genre);
     const genreId = selectedGenre.id;
 
-    const moviesUrl = `https://api.themoviedb.org/3/discover/movie?` +
-      `api_key=${process.env.REACT_APP_TMDB_API_KEY}&` +
-      `language=en-US&sort_by=popularity.desc&` +
+    const moviesUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}` +
+      `&language=en-US&sort_by=popularity.desc&` +
       `with_genres=${genreId}&` +
       `primary_release_date.gte=${year.value.min}-01-01&` +
       `primary_release_date.lte=${year.value.max}-12-31&` +
@@ -65,13 +81,52 @@ class Main extends React.Component {
       `vote_average.lte=${rating.value.max}&` +
       `with_runtime.gte=${runtime.value.min}&` +
       `with_runtime.lte=${runtime.value.max}&` +
-      `page=1&`;
+      `page=${page}`;
 
     this.setState({ moviesUrl });
   }
 
   onSearchButtonClick = () => {
+    this.setState({page: 1});
     this.generateUrl();
+  }
+
+  fetchMovies = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.storeMovies(data))
+      .catch(error => console.log(error));
+  }
+
+  storeMovies = data => {
+    const movies = data.results.map(result => {
+      const {
+        vote_count,
+        id,
+        genre_ids,
+        poster_path,
+        title,
+        vote_average,
+        release_date
+      } = result;
+      return { vote_count, id, genre_ids, poster_path, title, vote_average, release_date };
+    });
+    this.setState({ movies, total_pages: data.total_pages });
+  };
+
+  onPageIncrease = () => {
+    const { page, total_pages } = this.state
+    const nextPage = page + 1;
+    if (nextPage <= total_pages) {
+      this.setState({ page: nextPage })
+    }
+  }
+
+  onPageDecrease = () => {
+    const nextPage = this.state.page - 1;
+    if ( nextPage > 0 ) {
+      this.setState({ page: nextPage })
+    }
   }
 
   render() {
@@ -84,7 +139,12 @@ class Main extends React.Component {
          onSearchButtonClick={this.onSearchButtonClick}
          {...this.state}
         />
-        <Movies url={this.state.moviesUrl} />
+        <Movies
+          movies={this.state.movies}
+          page={this.state.page}
+          onPageIncrease={this.onPageIncrease}
+          onPageDecrease={this.onPageDecrease}
+        />
       </section>
     )
   }
--- src/main/movies/Movies.js
+++ src/main/movies/Movies.js
@@ -3,51 +3,26 @@
 import React from "react";
 import "./Movies.css";
 import MovieListItem from "./components/MovieListItem";
+import Button from "../navigation/components/Button";
 
-class Movies extends React.Component {
-  state = {
-   movies: []
-  }
-
-  componentDidMount() {
-    this.fetchMovies(this.props.url);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.url !== nextProps.url) {
-      this.fetchMovies(nextProps.url);
-    }
-  }
-
-  fetchMovies = (url) => {
-    fetch(url)
-      .then(response => response.json())
-      .then(data => this.storeMovies(data))
-      .catch(error => console.log(error));
-  }
-
-  storeMovies = data => {
-   const movies = data.results.map( result => {
-    const { vote_count, id, genre_ids, poster_path, title, vote_average, release_date } = result;
-    return { vote_count, id, genre_ids, poster_path, title, vote_average, release_date };
-   });
-
-   this.setState({ movies });
-  }
-
-  render() {
-    return (
-      <section>
-        <ul className="movies">
-          {
-            this.state.movies.map( movie => (
-              <MovieListItem key={movie.id} movie={movie} />
-            ))
-          }
-        </ul>
-      </section>
-    )
-  }
-}
+const Movies = ({
+  movies,
+  page,
+  onPageIncrease,
+  onPageDecrease,
+  }) => (
+    <section>
+      <ul className="movies">
+        {movies.map( movie => (
+          <MovieListItem key={movie.id} movie={movie} />
+        ))}
+      </ul>
+      <div className="pagination">
+        <Button onClick={onPageDecrease}>Previous</Button>
+        <span>{`Page ${page}`}</span>
+        <Button onClick={onPageIncrease}>Next</Button>
+      </div>
+    </section>
+)
 
 export default Movies;
