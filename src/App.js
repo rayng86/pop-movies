5910b6da5f1dcdb6bacb46f217154a0e35043a0b
Raymond Ng <rayng86@gmail.com>
2018-02-24 18:25:07 -0500

    update app.js to use browerrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./header/Header";
 import Main from "./main/Main";
+import NotFound from "./NotFound";
 import './App.css';
 
 class App extends Component {
   render() {
     // console.log('API key:', process.env.REACT_APP_TMDB_API_KEY);
     return (
-      <div className="App">
-       <Header />
-       <Main />
+      <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Main} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
+    </BrowserRouter>
     );
   }
 }
